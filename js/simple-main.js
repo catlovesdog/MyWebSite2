e9bc5521756bb3f106cfb2f1141ed0eff06f831e
catlovesdog <54089918+catlovesdog@users.noreply.github.com>
2025-02-20 14:52:34 -0500

    Update simple-main.js

--- js/simple-main.js
+++ js/simple-main.js
@@ -80,8 +80,7 @@
 		let imgElem;
 		for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
 			imgElem = new Image();
-			imgElem.src = `/MyWebSite2/video/002/IMG_${7027 + i}.jpg`;
-			
+			imgElem.src = `https://raw.githubusercontent.com/catlovesdog/MyWebSite2/master/video/002/IMG_${7027 + i}.jpg`;
 			sceneInfo[0].objs.videoImages.push(imgElem);
 		}
 	}
